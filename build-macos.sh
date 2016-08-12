4de4c343405bc06c61610856b82df6a14b3d16b7
greenygh0st <dale.myszewski@gmail.com>
2016-08-12 10:41:53 -0500

    Added macOS build script

---
+++ build-macos.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ $UID != 0]
+then
+  echo "Please run this script with sudo. It won't make the sandwich otherwise."
+  echo "sudo $0 $*"
+  exit 1
+fi
+
+xcode-select --install
+gcc --version
+
+#install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+which brew
+
+#install rbenv
+brew update
+brew install rbenv ruby-build rbenv-gem-rehash
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+#install ruby
+rbenv install -v 2.3.1
+rbenv global 2.3.1
+
+#install Git from brew
+#brew install git
+
+ruby -v
+
+echo "gem: --no-document" > ~/.gemrc
+
+gem install bundler
+gem install rails
+rbenv rehash
+
+rails -v

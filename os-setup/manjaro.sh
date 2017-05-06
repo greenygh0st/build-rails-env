248a967f53b583be8c06e17ecf7cc48b60881d5e
Dale Myszewski <greenygh0st@users.noreply.github.com>
2017-05-06 11:22:02 -0500

    Update manjaro.sh

--- os-setup/manjaro.sh
+++ os-setup/manjaro.sh
@@ -1,9 +1,3 @@
-# first fix our mirror to a solid local mirror
-# I have been a lot of problems lately with other mirrors
-echo "# no frills, no junk" | sudo tee /etc/pacman.d/mirrorlist > /dev/null
-echo "# USA" | sudo tee /etc/pacman.d/mirrorlist > /dev/null
-echo "Server = http://mirror.clarkson.edu/manjaro/stable/$repo/$arch" | sudo tee --append /etc/pacman.d/mirrorlist > /dev/null
-
 # make sure you already have nodejs installed
 sudo pacman -Syy
 sudo pacman -Sy nodejs

ffbf0b8026df43d50364989f4bf2b40e890aa70c
Dale Myszewski <greenygh0st@users.noreply.github.com>
2017-05-06 22:09:50 -0500

    Update manjaro.sh

--- os-setup/manjaro.sh
+++ os-setup/manjaro.sh
@@ -1,6 +1,6 @@
 # make sure you already have nodejs installed
 sudo pacman -Syy
-sudo pacman -Sy nodejs npm dnsutils vi youtube-dl curl kdenlive pgadmin3 teamspeak3 redshift
+sudo pacman -Sy nodejs npm dnsutils vi youtube-dl curl kdenlive pgadmin3 teamspeak3 redshift keepassx2
 
 # upgrade everything else
 sudo pacman -Su

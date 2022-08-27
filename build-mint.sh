61871cf6a51fdf2a27cb8d2409544708b1d313c9
Dale Myszewski <greenygh0st@users.noreply.github.com>
2022-08-27 14:57:15 -0500

    Update build-mint.sh

--- build-mint.sh
+++ build-mint.sh
@@ -12,7 +12,7 @@ sudo apt-get install nodejs
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
 cd
-git clone git://github.com/sstephenson/rbenv.git .rbenv
+git clone git://github.com/rbenv/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 
